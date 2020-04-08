fe1acee8b42d31919b3396df1e4d5a71d674c045
angelod1as <angelo@angelodias.com.br>
2020-04-08 15:15:02 -0300

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const cors = require('cors');
+const { uuid, isUuid } = require("uuidv4");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const projects = [];
+
+function logRequests(request, response, next) {
+  const { method, url } = request;
+  const logLabel = `[${method.toUpperCase()}] ${url}`
+  console.log(logLabel);
+  return next();
+}
+
+function validateProjectId(request, response, next) {
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: 'Invalid project ID.'});
+  }
+
+  return next();
+}
+
+app.use(logRequests);
+app.use('/projects/:id', validateProjectId);
+
+app.get('/projects', (req, res) => {
+  const { title } = req.query;
+
+  const results = title
+    ? projects.filter(project => project.title.includes(title))
+    : projects;
+
+  return res.json(results);
+})
+
+app.post('/projects', (req, res) => {
+  const { title, owner } = req.body;
+  const project = { id: uuid(), title, owner }
+
+  projects.push(project);
+
+  return res.json(project);
+})
+
+app.put('/projects/:id', (req, res) => {
+  const { id }  = req.params;
+  const { title, owner } = req.body;
+
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if (projectIndex < 0) {
+    return res.status(400).json({ error: 'project not found' });
+  }
+
+  const project = {
+    id,
+    title,
+    owner
+  }
+
+  projects[projectIndex] = project;
+
+  return res.json(project);
+})
+
+app.delete('/projects/:id', (req, res) => {
+  const { id } = req.params;
+
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if (projectIndex < 0) {
+    return res.status(400).json({ error: 'project not found' });
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return res.status(204).send();
+})
+
+
+app.listen(3333, () => {
+  console.log('🚀 Back-end started!');
+});
---
+++ frontent/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index'),
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node-modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude: /node-modules/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' }
+        ]
+      },
+      {
+        test: /.*\.(gif|png|jpe?g)$/i,
+        exclude: /node-modules/,
+        use: {
+          loader: 'file-loader'
+        }
+      },
+    ],
+  }
+}
